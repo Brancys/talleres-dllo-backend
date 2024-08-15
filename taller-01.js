711e7dcbd2c16dce1ed659c925bf9b77aea3b979
brancysb <luisasuarez031@gmail.com>
2024-08-14 22:52:15 -0500

    taller-01.js

--- taller-01.js
+++ taller-01.js
@@ -1,24 +1,24 @@
-function convertidorTemp(c){
-	return (1.8*c)+32
+function convertidorTemp(c) {
+  return 1.8 * c + 32;
 }
 
 //en signo colocar 1 o -1, si se quiere obtener la solución positiva o negativa respectivamente.
-function resolvedor(a,b,c,signo){ 
-	return (-1*b + signo*(b*b - 4*a*c)**(1/2))/(2*a)  
+function resolvedor(a, b, c, signo) {
+  return (-1 * b + signo * (b * b - 4 * a * c) ** (1 / 2)) / (2 * a);
 }
 
-function mejorParidad(n){
-	return !(n%2)
+function mejorParidad(n) {
+  return !(n % 2);
 }
 
-function peorParidad(n){
-	par = true
-	for (let i = 1; i <= n; i++) {  
-  	if(par == false){
-    	par = true
-    }else{
-    	par = false
+function peorParidad(n) {
+  par = true;
+  for (let i = 1; i <= n; i++) {
+    if (par == false) {
+      par = true;
+    } else {
+      par = false;
     }
   }
-  return par
+  return par;
 }
