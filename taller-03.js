7810e83324da4460f4b07e9f48981eebc80704f5
Brancys Barrios Suarez <62186502+Brancys@users.noreply.github.com>
2024-08-31 22:10:46 -0500

    punto1 taller 3

--- taller-03.js
+++ taller-03.js
@@ -1,1 +1,16 @@
+function desglosarString(cadena,param) {
+  cad = cadena.replace(/\s+/g, "").split('')
+  vocal = ['a','e','i','o','u']
+  numVocal = 0
+  for (let i = 0; i < cad.length; i++) {
+    if (vocal.includes(cad[i])) {
+      numVocal++
+    }    
+  }
+  if (param === 'vocales') {
+    return numVocal
+  }  
+  return cad.length - numVocal
+}
 
+console.log(desglosarString('cadena','vocales'));
